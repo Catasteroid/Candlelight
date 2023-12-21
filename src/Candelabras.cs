d7df9e6c383c1d0ab12703ebe4b3e63e1ff33197
Catasteroid <howescaleb@gmail.com>
2023-12-21 22:20:35 +0000

    simple/complex candelabara update
    added complex candelabra class and changed original class to BlockSimpleCandelabra

--- src/Candelabras.cs
+++ src/Candelabras.cs
@@ -17,7 +17,7 @@ using System.Threading.Tasks;
 namespace Candlelight
 {
 
-	public class BlockCandelabra : Block
+	public class BlockSimpleCandelabra : Block
     {
 	
 		private int numCandles;
@@ -140,4 +140,207 @@ namespace Candlelight
         }
     }
 
+	public class BlockCandelabra : Block
+	{
+	
+		private int maxCandles;
+	
+		public int CandleCount
+        {
+            get
+            {
+                switch (CodeWithParts("candlenumber"))
+                {
+                    case "candle0":
+                        return 0;
+                    case "candle1":
+                        return 1;
+                    case "candle2":
+                        return 2;
+                    case "candle3":
+                        return 3;
+                    case "candle4":
+                        return 4;
+                    case "candle5":
+                        return 5;
+                    case "candle6":
+                        return 6;
+                    case "candle7":
+                        return 7;
+                    case "candle8":
+                        return 8;
+					case "candle9":
+                        return 9;
+					case "candle10":
+                        return 10;
+					case "candle11":
+                        return 11;
+					case "candle12":
+                        return 12;
+                    case "candle13":
+                        return 13;
+                    case "candle14":
+                        return 14;
+                    case "candle15":
+                        return 15;
+                    case "candle16":
+                        return 16;
+                    case "candle17":
+                        return 17;
+                    case "candle18":
+                        return 18;
+					case "candle19":
+                        return 19;
+					case "candle20":
+                        return 20;
+					case "candle21":
+                        return 21;
+					case "candle22":
+                        return 22;
+                    case "candle23":
+                        return 23;
+                    case "candle24":
+                        return 24;
+                    case "candle25":
+                        return 25;
+                }
+                return -1;
+            }
+        }
+		
+		public bool Lit
+        {
+            get { return Variant["state"] == "lit"; }
+        }
+	
+		public override bool OnBlockInteractStart(IWorldAccessor world, IPlayer byPlayer, BlockSelection blockSel)
+        {
+			maxCandles = Block.Attributes?["maxCandles"]?.AsInt() ?? 1;
+            int candlecount = CandleCount;
+            ItemStack itemstack = byPlayer.InventoryManager.ActiveHotbarSlot?.Itemstack;
+
+			// If they're not holding shift down place a candle if they're holding on or toggle lit/unlit if they're not
+			if (!byEntity.Controls.ShiftKey)
+			{
+				// Attempt to add a candle to the candelabra
+				if (itemstack != null && itemstack.Collectible.Code.Path == "candle" && CandleCount != 8)
+				{
+					if (byPlayer != null && byPlayer.WorldData.CurrentGameMode == EnumGameMode.Survival)
+					{
+						byPlayer.InventoryManager.ActiveHotbarSlot.TakeOut(1);
+						Block block = world.GetBlock(CodeWithParts("candlenumber",GetNextCandleCount()));
+						world.BlockAccessor.ExchangeBlock(block.BlockId, blockSel.Position);
+						world.BlockAccessor.MarkBlockDirty(blockSel.Position);
+
+						return true;
+					}
+				}
+				// Attempt to toggle between lit and unlit
+				else if (itemstack == null || itemstack.Collectible.Code.Path != "candle")
+				{
+					if (!Lit)
+					{
+						Block filledBlock = world.GetBlock(CodeWithVariant("state", "lit"));
+						world.BlockAccessor.ExchangeBlock(filledBlock.BlockId, blockSel.Position);
+						return false;
+					} 
+					else 
+					{
+						Block filledBlock = world.GetBlock(CodeWithVariant("state", "unlit"));
+						world.BlockAccessor.ExchangeBlock(filledBlock.BlockId, blockSel.Position);
+						return true;
+					}
+				}
+			}
+			else
+			{
+				// Attempt to remove a candle from the candelabra if it has any
+				if (CandleCount > 0)
+				{
+					if (byPlayer != null && byPlayer.WorldData.CurrentGameMode == EnumGameMode.Survival)
+					{
+						ItemStack stack = new ItemStack(world.GetItem(new AssetLocation("candle")));
+						if (byPlayer.InventoryManager.TryGiveItemstack(stack, true))
+						{
+							Block block = world.GetBlock(CodeWithParts("candlenumber",GetPreviousCandleCount()));
+							world.BlockAccessor.ExchangeBlock(block.BlockId, blockSel.Position);
+							world.BlockAccessor.MarkBlockDirty(blockSel.Position);
+							
+							return true;
+						}
+					}
+				}
+			}
+            return false;
+        }
+
+        string GetNextCandleCount()
+        {
+            if (CandleCount != maxCandles)
+                return $"candle{CandleCount + 1}";
+            else 
+                return "";
+        }
+
+		string GetPreviousCandleCount()
+        {
+            if (CandleCount != 0)
+                return $"candle{CandleCount - 1}";
+            else 
+                return "";
+        }
+
+        public override WorldInteraction[] GetPlacedBlockInteractionHelp(IWorldAccessor world, BlockSelection selection, IPlayer forPlayer)
+        {
+            if (CandleCount == maxCandles)
+			{
+				return new WorldInteraction[]
+				{
+					new WorldInteraction()
+					{
+						ActionLangCode = "blockhelp-chandelier-removecandle",
+						HotKeyCode = "shift",
+						MouseButton = EnumMouseButton.Right,
+						Itemstacks = new ItemStack[] { new ItemStack(world.GetItem(new AssetLocation("candle"))) }
+					}
+				}.Append(base.GetPlacedBlockInteractionHelp(world, selection, forPlayer));
+			}
+			else if (CandleCount == 0)
+			{
+				return new WorldInteraction[]
+				{
+					new WorldInteraction()
+					{
+						ActionLangCode = "blockhelp-chandelier-addcandle",
+						MouseButton = EnumMouseButton.Right,
+						Itemstacks = new ItemStack[] { new ItemStack(world.GetItem(new AssetLocation("candle"))) }
+					}
+				}.Append(base.GetPlacedBlockInteractionHelp(world, selection, forPlayer));
+			}
+			else
+			{
+				return new WorldInteraction[]
+				{
+					new WorldInteraction()
+					{
+						ActionLangCode = "blockhelp-chandelier-addcandle",
+						MouseButton = EnumMouseButton.Right,
+						Itemstacks = new ItemStack[] { new ItemStack(world.GetItem(new AssetLocation("candle"))) }
+					},
+					new WorldInteraction()
+					{
+						ActionLangCode = "blockhelp-chandelier-removecandle",
+						MouseButton = EnumMouseButton.Right,
+						Itemstacks = new ItemStack[] { new ItemStack(world.GetItem(new AssetLocation("candle"))) }
+					},
+					new WorldInteraction()
+					{
+						ActionLangCode = "blockhelp-chandelier-togglecandles",
+						HotKeyCode = "shift",
+						MouseButton = EnumMouseButton.Right
+					}
+				}.Append(base.GetPlacedBlockInteractionHelp(world, selection, forPlayer));
+			}
+		}					
+	}
 }
